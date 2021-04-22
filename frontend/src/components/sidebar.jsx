f25e62b04bd99adb3ee9efa733e1964822934fcf
jbofill10 <juan.bofill10@gmail.com>
2021-04-21 22:36:50 -0400

    Add sidebar transition

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import { SidebarData } from './SidebarData.jsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default class Sidebar extends React.Component {
 
 	constructor(props){
 		super(props);
-		this.state={}
+        this.state={
+            'showSidebar': false
+        }
 	}
+
+    barsClick = () => {
+        this.state.showSidebar = this.setState({'showSidebar': !this.state.showSidebar})
+    }
+
 	render(){
         return(
-            <div className='Sidebar'>
-                <ul>
-                    {SidebarData.map((val, key) => {
-                        return(
-                            <li key={key}> 
-                                <div>{val.icon}</div> 
-                            </li>
-                        )
-                    })}
-                </ul>
+            <div>
+                <div className='NavBar'>
+                    <FontAwesomeIcon onClick={this.barsClick} icon={faBars} />
+                </div>
+
+                <div className='Sidebar'>
+                    <div className={this.state.showSidebar ? 'SidebarOpen' : 'SidebarClosed'}>
+                        <ul className='SideBarList'>
+                            {SidebarData.map((val, key) => {
+                                return(
+                                    <li className='SidebarRow' key={key}> 
+                                        <div>{val.icon}</div> 
+                                        <div className='SidebarText'>{val.title}</div>
+                                    </li>
+                                )})}
+                        </ul>
+                    </div>
+                </div>
             </div>
 		)
 	}
