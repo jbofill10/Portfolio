b58ba1e633899757c46f6ba4820eaedab0430ed7
jbofill10 <juan.bofill10@gmail.com>
2021-06-27 03:22:43 -0400

    add front page work

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,11 +1,37 @@
 import Sidebar from '../components/sidebar.jsx'
 
 function App() {
-  return (
-    <div className="App">
-        <Sidebar/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Sidebar/>
+			<div className='AboutLayout'>
+				<div className='IntroText'>
+					<div className='Introduction'>
+						Hi, my name is
+						<div className='Name'>Juan Bofill</div>
+
+						<div className='Description'>
+							I am a Software Engineer that loves to work on cool projects in my free time to
+							grow in different areas of Software Engineering and Machine Learning
+						</div>
+					</div>
+				</div>
+
+				<div className='Separator' />
+
+				<div className='ToolIntro' style={{
+					fontSize: 20,
+					marginBottom: "auto"
+					}}>
+					Here's what I like working with
+				</div>
+
+				<div className='Tools'>
+
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
